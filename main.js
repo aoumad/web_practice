724d35af31a38e4d344178c347e3476126728581
Abderazzak Oumad <96908237+aoumad@users.noreply.github.com>
2022-12-25 14:42:34 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-window.onload = function()
-{
-    document.querySelector("h1").style.color = 'Blue';
-}; 
+// window.onload = function()
+// {
+//     document.querySelector("h1").style.color = 'Orange';
+// }; 
+
+// window.alert("Hello from main.js");
+
+// window.write("<h1>Hello <span>Page</span><h1>");
+
+// console.log("hahahah hello again from JS  file");
+
+// console.log("Log");
+// console.error("error");
+// console.warn("warn");
+console.table(["ayman", "hamza", "soufiane"]);
+
+console.log("Hello from %cmain.js %cFile", "color: red; front-size: 40px", "color: blue; front-size: 40px");
