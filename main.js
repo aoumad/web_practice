a40edf0b2e09853a0dc5f9e8239d530f1b9a43e3
Abderazzak Oumad <96908237+aoumad@users.noreply.github.com>
2022-12-25 23:15:40 +0100

    updates of the day

--- main.js
+++ main.js
@@ -12,6 +12,28 @@
 // console.log("Log");
 // console.error("error");
 // console.warn("warn");
-console.table(["ayman", "hamza", "soufiane"]);
+// console.table(["ayman", "hamza", "soufiane"]);
 
-console.log("Hello from %cmain.js %cFile", "color: red; front-size: 40px", "color: blue; front-size: 40px");
+// console.log("Hello from %cmain.js %cFile", "color: red; front-size: 40px", "color: blue; front-size: 40px");
+
+// variables part
+
+var name = "Ahmed";
+var age = 20;
+var isMale = true;
+var isFemale = false;
+var isMarried = false;
+var isSingle = true; 
+
+console.log(name);
+console.log(age);
+console.log(isMale);
+console.log(isFemale);
+console.log(isMarried);
+console.log(isSingle);
+console.log(hello);
+
+hello.innerHTML = "Option";
+
+let a = 22;
+a = 66;
