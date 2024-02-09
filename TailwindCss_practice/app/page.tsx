8cfd3b62fb64d97f1ee3d605b8461b5d007cc85b
Abderazzak Oumad <aoumad@e2r7p12.1337.ma>
2024-02-09 23:43:14 +0100

    learning tailwind is fun

---
+++ TailwindCss_practice/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+      <h1 className="bg-green-600 text-white border-4 border-vuejs">
+        {/* Hello world! */}
+        <p className="text-xs lg:text-3x">Hello long sentence</p>
+      </h1>
+    )
+  }
---
+++ TailwindCss_practice/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        vuejs: "#19e61d",
+      },
+    },
+  },
+  plugins: [],
+}
+
